950542cae076305570b502fc0e81dacbe119a745
Ryan Williams <rpwilliams96@gmail.com>
2017-04-12 21:42:57 -0500

    Added new idea view and MySQL database

---
+++ db/tables.sql
@@ -0,0 +1,10 @@
+/* Delete the table if it already exists */
+DROP TABLE IF EXISTS Ideas;
+
+/* Create the schema */
+CREATE TABLE Ideas(
+					idea_ID int not null primary key,
+					name varchar(50) not null,
+					description  varchar(2000) not null,
+					category varchar(50) not null
+				  );
---
+++ db/test_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO 'Ideas' ('idea_ID','name','description','category') VALUES (1,"Test", "jfasfsafj", "Test Category");
