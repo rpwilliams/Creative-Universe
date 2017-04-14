f1839883bef49e3dcb136db598b0f871a81abb33
Ryan Williams <rpwilliams96@gmail.com>
2017-04-13 22:11:15 -0500

    Added untested MySQL connection to API

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, url_for
+from flaskext.mysql import MySQL
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'tOrpedo02'
+app.config['MYSQL_DATABASE_DB'] = 'Ideas'
+app.config['MYSQL_DATABASE_HOST'] = 'rpwilliams96-desktop'
+mysql.init_app(app)
 
 app = Flask(__name__)
+mysql = MySQL()
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
 	return render_template('add.html')
+
+@app.route('/ideas', methods=['GET', 'POST'])
+def ideas():
+	return render_template('ideas.html')
 
 if __name__ == '__main__':
   app.run(debug = True)
